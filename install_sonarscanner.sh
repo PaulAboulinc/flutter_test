876d66b8fd9d32dd6805ad6e540af7396f01dcf8
Paul Aboulinc <paul.aboulinc@outlook.fr>
2022-02-10 16:00:50 +0100

    fix install sonar scanner

--- install_sonarscanner.sh
+++ install_sonarscanner.sh
@@ -1,6 +1,9 @@
 curl -o sonarscanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip
 unzip sonarscanner.zip -d sonarscanner
-cd sonarscanner/sonar*
+cd sonarscanner/sonar* || exit
 rm conf/sonar-scanner.properties
-mv ../../sonar-scanner.properties conf
-export PATH=bin:$PATH
+cp ../../sonar-scanner.properties conf
+pwd=`pwd`
+export PATH=$(pwd)/bin:$PATH
+cd ../../
+rm sonarscanner.zip
