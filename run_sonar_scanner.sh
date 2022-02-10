7f727457e2ae79ee90d4ad9b469725008a843cfd
Paul Aboulinc <paul.aboulinc@outlook.fr>
2022-02-10 16:21:44 +0100

    test github secret

--- run_sonar_scanner.sh
+++ run_sonar_scanner.sh
@@ -1,8 +1,8 @@
 if ! command -v sonar-scanner &> /dev/null
 then
-    echo "Installation of sonar-scanner : "
-    
-    echo "Download and unzip sonar-scanner archive : "
+    echo "Installation of sonar-scanner"
+
+    echo "Download and unzip sonar-scanner archive"
     curl -o sonarscanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip
     unzip sonarscanner.zip -d sonarscanner
 
@@ -11,14 +11,14 @@ then
     rm conf/sonar-scanner.properties
     cp ../../sonar-scanner.properties conf
 
-    echo "Add sonar-scanner binaries to PATH : "
+    echo "Add sonar-scanner binaries to PATH"
     pwd=`pwd`
     export PATH=$(pwd)/bin:$PATH
 
-    echo "Cleaning up for local environment : "
+    echo "Cleaning up for local environment"
     cd ../../
     rm sonarscanner.zip
 fi
 
-echo "Run sonar-scanner : "
+echo "Run sonar-scanner"
 sonar-scanner
